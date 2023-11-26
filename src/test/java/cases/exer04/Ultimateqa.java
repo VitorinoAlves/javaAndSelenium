02d2a889e9a5a4f8e2d822931c91c77f325d1965
VitorinoAlves <vitorino.17alexandre@gmail.com>
2023-11-26 12:26:40 -0300

    Finishing task 4

--- src/test/java/cases/exer04/Ultimateqa.java
+++ src/test/java/cases/exer04/Ultimateqa.java
@@ -2,10 +2,15 @@ package cases.exer04;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import pages.UltimateHome;
 import utils.Driver;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Ultimateqa extends Driver {
     /*Localize o componente Clickable Icon e validar o redirecionamento para a próxima página e voltar para a home*/
@@ -26,10 +31,73 @@ public class Ultimateqa extends Driver {
 
         ultimateHome.inputContactName("Test");
         ultimateHome.inputContactEamil("test@test.com");
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         ultimateHome.clickOnEmailMeButton();
 
         assertEquals(ultimateHome.getEmailSentMessage(), "Thanks for contacting us");
 
+    }
+
+    /*Selecionar um radio button (male, female, other)*/
+    @Test
+    public void selectOneRadioButton() throws InterruptedException{
+        UltimateHome ultimateHome = new UltimateHome(driver);
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+
+        ultimateHome.clickOnFemaleRadioButton();
+        Thread.sleep(500);
+        assertTrue(ultimateHome.getFemaleRadioButtonStatus());
+    }
+
+    /*Selecionar os checkbox I have a bike e I have a car*/
+    @Test
+    public void selectCheckboxes() throws InterruptedException{
+        UltimateHome ultimateHome = new UltimateHome(driver);
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+
+        ultimateHome.checkIHaveBikeCheckbox();
+        ultimateHome.checkIHaveCarCheckbox();
+        Thread.sleep(500);
+        assertTrue(ultimateHome.getIHaveBikeCheckboxStatus());
+        assertTrue(ultimateHome.getIHaveCarCheckboxStatus());
+    }
+
+    /*Selecionar dropdown Saab depois selecionar Audi*/
+    @Test
+    public void selectOptionsDropdown(){
+        UltimateHome ultimateHome = new UltimateHome(driver);
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+
+        ultimateHome.selectCar("saab");
+        ultimateHome.selectCar("audi");
+        assertEquals(ultimateHome.getSelectedOption(), "Audi");
+    }
+
+    /*Selecionar Tab 2 Validar seu conteúdo
+        Selecionar tab 1 validar seu conteúdo
+    */
+    @Test
+    public void changeTab() throws InterruptedException{
+        UltimateHome ultimateHome = new UltimateHome(driver);
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        WebElement tabTwoOption = driver.findElement(By.xpath("//li[@class='et_pb_tab_1']/a"));
+        WebElement tabTwoContent = driver.findElement(By.xpath("//div[contains(text(),'Tab 2 content')]"));
+        WebElement tabOneContent = driver.findElement(By.xpath("//div[contains(text(),'tab 1 content')]"));
+
+
+        driver.findElement(By.xpath("//div[@class='et_pb_tab_content']")).click();
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(By.xpath("//ul[@class='et_pb_tabs_controls clearfix']")));
+        Thread.sleep(2000);
+
+        tabTwoOption.click();
+        Thread.sleep(1000);
+        assertTrue(tabTwoContent.isDisplayed());
+
+        WebElement tabOneOption = driver.findElement(By.xpath("//li[@class='et_pb_tab_0']/a"));
+        tabOneOption.click();
+        Thread.sleep(1000);
+        assertTrue(tabOneContent.isDisplayed());
     }
 }
--- src/test/java/pages/UltimateHome.java
+++ src/test/java/pages/UltimateHome.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class UltimateHome {
     private WebDriver driver;
@@ -13,6 +15,14 @@ public class UltimateHome {
     By emailField = By.xpath("//input[@id='et_pb_contact_email_0']");
     By emailMeButton = By.xpath("//button[@name='et_builder_submit_button']");
     By emailSentMessage = By.xpath("//div[@class='et-pb-contact-message']//p");
+    By femaleRadioButton = By.xpath("//input[@value='female']");
+    By iHaveBikeCheckbox = By.xpath("//input[@value='Bike']");
+    By iHaveCarCheckbox = By.xpath("//input[@value='Car']");
+    By carDropdown = By.xpath("//option[@value='volvo']/..");
+    By tabOne = By.xpath("//li[@class='et_pb_tab_0']");
+    By tabTwo = By.xpath("//li[@class='et_pb_tab_1']/a");
+    By tabText = By.xpath("//div[@class='et_pb_tab_content']");
+    By listOftabs = By.xpath("//ul[@class='et_pb_tabs_controls clearfix']");
 
     public void clickClickableIcon(){
         driver.findElement(clickableIcon).click();
@@ -33,5 +43,51 @@ public class UltimateHome {
     }
     public String getEmailSentMessage(){
         return driver.findElement(emailSentMessage).getText();
+    }
+
+    public void clickOnFemaleRadioButton(){
+        driver.findElement(femaleRadioButton).click();
+    }
+
+    public boolean getFemaleRadioButtonStatus(){
+        return driver.findElement(femaleRadioButton).isSelected();
+    }
+
+    public void checkIHaveBikeCheckbox(){
+        driver.findElement(iHaveBikeCheckbox).click();
+    }
+    public void checkIHaveCarCheckbox(){
+        driver.findElement(iHaveCarCheckbox).click();
+    }
+    public boolean getIHaveBikeCheckboxStatus(){
+        return driver.findElement(iHaveBikeCheckbox).isSelected();
+    }
+    public boolean getIHaveCarCheckboxStatus(){
+        return driver.findElement(iHaveCarCheckbox).isSelected();
+    }
+
+    public void selectCar(String option){
+        Select carDropdownObject = new Select(driver.findElement(carDropdown));
+        carDropdownObject.selectByValue(option);
+    }
+    public String getSelectedOption(){
+        Select carDropdownObject = new Select(driver.findElement(carDropdown));
+        return carDropdownObject.getFirstSelectedOption().getText();
+    }
+
+    public void clickOnTabOne(){
+        driver.findElement(tabOne).click();
+    }
+    public void clickOnTabTwo(){
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click()", driver.findElement(tabTwo));
+        //driver.findElement(tabTwo).click();
+    }
+    public String getTabText(){
+        return driver.findElement(tabText).getText();
+    }
+    public void scrollToTabList(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(listOftabs));
+        driver.findElement(tabText).click();
     }
 }
